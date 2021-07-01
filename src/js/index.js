0bf11662c401f4709ecc01b5af491589212e664b
Anndersonn <slobodjanik1997@gmail.com>
2021-07-01 10:49:03 +0200

    refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,30 @@
-const test = document.querySelectorAll('#contacts')
-const test2 = document.querySelector('.burger__contacts')
+// mobile contact icons
+const icons = document.querySelectorAll('#contacts')
+const btnContacts = document.querySelector('.burger__contacts')
 
-console.log(test)
-console.log(test2)
-
-// for (let i = 0; i < test.length; i++) {
-//     console.log[i]
-//     test2[i].addEventListener('click', () => {
-//         console.log(test[i])
-//         test2[i].classList.toggle('active__contacts')
-//     })
-// }
-
-test2.addEventListener('click', () => {
-    for (let i = 0; i < test.length; i++) {
-        test[i].classList.toggle('active__contacts')
+btnContacts.addEventListener('click', () => {
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].classList.toggle('active__contacts')
 
     }
 })
+//  work section rotate effect
+const workItems = document.querySelectorAll('.work__item')
+
+
+for (let i = 0; i < workItems.length; i++) {
+    workItems[i].addEventListener('click', () => {
+        workItems[i].classList.toggle('work__active')
+    })
+}
+
+// burger menu
+
+const burger = document.getElementById('burger')
+const menu = document.getElementById('menu')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger__active')
+    menu.classList.toggle('menu__active')
+})
+
